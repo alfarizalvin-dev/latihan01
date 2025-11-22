236b9230dd7050e560ed2be5243792efa8bc3d37
Alvin Yunas <alfarizalvin@gmail.com>
2025-11-22 16:33:20 +0700

    Menambahkan file page1, page2, page3

---
+++ pages/page2.py
@@ -0,0 +1,15 @@
+imprt streamlit as st
+import plotly.espress as px
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("Data Visualization")
+
+#Generate some data
+x = np.license(0, 10, 100)
+y = np.sin(x)
+# Plot Data
+fig, ax = plt.subplots()
+ax.plot(x,y)
+#Display the plot
+st.pyplot(fig)
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome to Alvin's Home Page")
+st.write("This page allows you to customize your preferences")
